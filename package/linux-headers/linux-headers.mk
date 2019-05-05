ff6f00ac3dc9a8d8eb893a28f69019131bffbdc9
iago <git@aeolio.de>
2019-05-05 15:35:47 +0200

    reduce amount of files unpacked as linux kernel headers

---
+++ package/linux-headers/linux-headers.mk
@@ -0,0 +1,7 @@
+################################################################################
+#
+# Fragment file to exclude source code from extracting
+#
+################################################################################
+
+LINUX_HEADERS_EXCLUDES = configs Documentation firmware samples *.bmp *.S Kconfig*
