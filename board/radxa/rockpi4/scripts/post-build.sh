b6f5ca98100ee289c55b713dd995058d2fd10d8e
iago <git@aeolio.de>
2019-06-09 17:48:41 +0200

    radxa changes to make rockchip kernel build

--- board/radxa/rockpi4/scripts/post-build.sh
+++ board/radxa/rockpi4/scripts/post-build.sh
@@ -5,6 +5,8 @@ set -eu # treat warnings as errors
 BOARD_DIR="$(dirname $0)"
 CONFIG_NAME=hw_intfc.conf
 DTB_NAME="$(basename $(awk -F '=' '/BR2_LINUX_KERNEL_INTREE_DTS_NAME/ {print $2}' ${BR2_CONFIG} | tr -d \").dtb)"
+LINUX_PATH="linux-$(awk -F '=' '/BR2_LINUX_KERNEL_VERSION/ {print $2}' ${BR2_CONFIG} | tr -d \")"
+OVERLAY_PATH=arch/arm64/boot/dts/rockchip/overlays-rockpi4
 
 #
 # copy extlinux to BINARIES_DIR and replace devicetree line
@@ -13,7 +15,14 @@ mkdir -p ${BINARIES_DIR}/extlinux
 sed -e "s|devicetree .*|devicetree /boot/${DTB_NAME}|" ${BOARD_DIR}/extlinux.conf > ${BINARIES_DIR}/extlinux/extlinux.conf
 
 #
-# Linux image install has placed the hardware configuration file in ${BINARIES_DIR}/overlays,
+# copy device tree overlays
+#
+mkdir -p ${BINARIES_DIR}/overlays
+cp ${BUILD_DIR}/${LINUX_PATH}/${OVERLAY_PATH}/*.dtbo ${BINARIES_DIR}/overlays/
+cp ${BUILD_DIR}/${LINUX_PATH}/${OVERLAY_PATH}/hw_intfc.conf ${BINARIES_DIR}/overlays/
+
+#
+# Previous step has placed the hardware configuration file in ${BINARIES_DIR}/overlays,
 # now transfer this file to ${BINARIES_DIR} and enable the ttyS2 overlay
 #
 sed -e 's|#.*=console-on-ttyS2|intfc:dtoverlay=console-on-ttyS2/|' \
