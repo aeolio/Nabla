552637f75382fa22e85d6f85a7b4d6b0d0bf18a9
iago <git@aeolio.de>
2019-06-10 07:32:47 +0200

    updated raspberry pi 32|64 linux versions

--- package/linux-headers/linux-headers.mk
+++ package/linux-headers/linux-headers.mk
@@ -4,4 +4,9 @@
 #
 ################################################################################
 
+# RT patch for Linux 4.14 contains also changes in sample code
+ifeq ($(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_4_14),y)
+LINUX_HEADERS_EXCLUDES = configs *.bmp
+else
 LINUX_HEADERS_EXCLUDES = configs samples *.bmp
+endif
