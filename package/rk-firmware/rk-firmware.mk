01004ac06846be2643ad1343ab0ead503a0c21cf
iago <git@aeolio.de>
2020-01-11 19:18:11 +0100

    rockpi4 with mainline kernel and uboot; rockchip firmware reverted to last working version

--- package/rk-firmware/rk-firmware.mk
+++ package/rk-firmware/rk-firmware.mk
@@ -4,16 +4,25 @@
 #
 ################################################################################
 
-RK_FIRMWARE_VERSION = 169dea138c76f06d1e06c43ff953636cf673e5b1
+RK_FIRMWARE_VERSION = ce80f14c683d1eb5d1b2af0360512480848ff6f9
 RK_FIRMWARE_SITE = $(call github,radxa,rkbin,$(RK_FIRMWARE_VERSION))
 RK_FIRMWARE_LICENSE = PROPRIETARY
 RK_FIRMWARE_LICENSE_FILES = 
 RK_FIRMWARE_INSTALL_IMAGES = YES
 RK_FIRMWARE_INSTALL_STAGING = NO
 RK_FIRMWARE_INSTALL_TARGET = NO
-RK_FIRMWARE_DEPENDENCIES = host-rk-firmware
+RK_FIRMWARE_DEPENDENCIES = host-rk-firmware host-uboot-tools
 
-# binary tools supplied by Rockchips for generating images
+RK_FIRMWARE_DDRFW = rk3399_ddr_800MHz_v1.20.bin
+RK_FIRMWARE_IDBLDR = rk3399_miniloader_v1.19.bin
+RK_FIRMWARE_SPILDR = rk3399_miniloader_spinor_v1.14.bin
+
+# Rockchip version
+# RK_FIRMWARE_TRUSTED_FW = $(@D)/RKTRUST/RK3399TRUST.ini
+# Radxa version
+RK_FIRMWARE_TRUSTED_FW = $(RK_FIRMWARE_PKGDIR)/trust.ini
+
+# binary tools supplied by Rockchip for generating images
 define HOST_RK_FIRMWARE_INSTALL_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/tools/firmwareMerger $(HOST_DIR)/bin/firmwareMerger
 	$(INSTALL) -D -m 0755 $(@D)/tools/loaderimage $(HOST_DIR)/bin/loaderimage
@@ -21,23 +30,20 @@ define HOST_RK_FIRMWARE_INSTALL_CMDS
 endef
 
 define RK_FIRMWARE_BUILD_CMDS
-	$(MKIMAGE) -n rk3399 -T rksd -d $(@D)/bin/rk33/rk3399_ddr_800MHz_v1.20.bin $(@D)/idbloader.img
-	cat $(@D)/bin/rk33/rk3399_miniloader_v1.19.bin >> $(@D)/idbloader.img
-	$(MKIMAGE) -n rk3399 -T rkspi -d $(@D)/bin/rk33/rk3399_ddr_800MHz_v1.20.bin $(@D)/idbloader-spi.img
-	cat $(@D)/bin/rk33/rk3399_miniloader_spinor_v1.14.bin >> $(@D)/idbloader-spi.img
-	sed -e 's|PATH=|PATH=$(@D)/|' $(RK_FIRMWARE_PKGDIR)/trust.ini > $(@D)/trust.ini
+	$(MKIMAGE) -n rk3399 -T rksd -d $(@D)/bin/rk33/$(RK_FIRMWARE_DDRFW) $(@D)/idbloader.img
+	cat $(@D)/bin/rk33/$(RK_FIRMWARE_IDBLDR) >> $(@D)/idbloader.img
+	$(MKIMAGE) -n rk3399 -T rkspi -d $(@D)/bin/rk33/$(RK_FIRMWARE_DDRFW) $(@D)/idbloader-spi.img
+	cat $(@D)/bin/rk33/$(RK_FIRMWARE_SPILDR) >> $(@D)/idbloader-spi.img
+	sed -e 's|PATH=|PATH=$(@D)/|g' $(RK_FIRMWARE_TRUSTED_FW) > $(@D)/trust.ini
 	$(@D)/tools/trust_merger --size 1024 1 $(@D)/trust.ini
 endef
 
 define RK_FIRMWARE_INSTALL_IMAGES_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/idbloader.img $(BINARIES_DIR)/u-boot/idbloader.img
 	$(INSTALL) -D -m 0644 $(@D)/idbloader-spi.img $(BINARIES_DIR)/u-boot/spi/idbloader-spi.img
-	# these are probably not needed
-	$(INSTALL) -D -m 0644 $(@D)/bin/rk33/rk3399_loader_v1.12.112.bin $(BINARIES_DIR)/u-boot/rk3399_loader_v1.12.112.bin
-	$(INSTALL) -D -m 0644 $(@D)/bin/rk33/rk3399_loader_spinor_v1.15.114.bin $(BINARIES_DIR)/u-boot/spi/rk3399_loader_spinor_v1.15.114.bin
-	# these are needed for SPI build
-	$(INSTALL) -D -m 0644 $(@D)/bin/rk33/rk3399_ddr_800MHz_v1.20.bin $(BINARIES_DIR)/firmware/rk3399_ddr_800MHz_v1.20.bin
-	$(INSTALL) -D -m 0644 $(@D)/bin/rk33/rk3399_miniloader_spinor_v1.14.bin $(BINARIES_DIR)/firmware/rk3399_miniloader_spinor_v1.14.bin
+	# next two are needed later for U-Boot build
+	$(INSTALL) -D -m 0644 $(@D)/bin/rk33/$(RK_FIRMWARE_DDRFW) $(BINARIES_DIR)/firmware/$(RK_FIRMWARE_DDRFW)
+	$(INSTALL) -D -m 0644 $(@D)/bin/rk33/$(RK_FIRMWARE_SPILDR) $(BINARIES_DIR)/firmware/$(RK_FIRMWARE_SPILDR)
 	$(INSTALL) -D -m 0644 $(@D)/trust.img $(BINARIES_DIR)/u-boot/trust.img
 endef
 
--- package/uboot/uboot.mk
+++ package/uboot/uboot.mk
@@ -6,15 +6,18 @@
 
 target = $(findstring rockpi4,$(BR2_DEFCONFIG))
 
-# Rockchip board has been selected
+### Rockchip board has been selected
 ifeq ($(target),rockpi4)
 
-# provides necessary binaries for image creation
+### provides necessary binaries for image creation
 UBOOT_DEPENDENCIES += rk-firmware
 
-# create boot images
-define UBOOT_BUILD_RK3399
+### create boot images
+define UBOOT_BUILD_DTB_IMAGE
 	$(HOST_DIR)/bin/loaderimage --pack --uboot $(@D)/u-boot-dtb.bin $(@D)/uboot.img 0x200000 --size 1024 1
+endef
+
+define UBOOT_BUILD_TRUST_IMAGE
 	sed -e 's|uboot.img|$(@D)/uboot.img|' \
 		-e 's|trust.img|$(BINARIES_DIR)/u-boot/trust.img|' \
 		-e 's|bin/rk33|$(BINARIES_DIR)/firmware|g' \
@@ -22,14 +25,22 @@ define UBOOT_BUILD_RK3399
 	$(HOST_DIR)/bin/firmwareMerger -P $(@D)/spi.ini $(@D)
 endef
 
-# install boot images
-define UBOOT_INSTALL_IMAGE_RK3399
+UBOOT_POST_BUILD_HOOKS += UBOOT_BUILD_DTB_IMAGE
+# trusted fimware generation fails and file is never used anyway
+# UBOOT_POST_BUILD_HOOKS += UBOOT_BUILD_TRUST_IMAGE
+
+### install boot images
+define UBOOT_INSTALL_IMAGES_UBOOT_IMAGE
 	$(INSTALL) -D -m 0644 $(@D)/uboot.img $(BINARIES_DIR)/u-boot/uboot.img
+endef
+
+define UBOOT_INSTALL_IMAGES_TRUST_IMAGE
 	$(INSTALL) -D -m 0644 $(@D)/Firmware.img $(BINARIES_DIR)/u-boot/spi/uboot-trust-spi.img
 	$(INSTALL) -D -m 0644 $(@D)/Firmware.md5 $(BINARIES_DIR)/u-boot/spi/uboot-trust-spi.img.md5
 endef
 
-UBOOT_POST_BUILD_HOOKS += UBOOT_BUILD_RK3399
-UBOOT_POST_INSTALL_IMAGES_HOOKS += UBOOT_INSTALL_IMAGE_RK3399
+UBOOT_POST_INSTALL_IMAGES_HOOKS += UBOOT_INSTALL_IMAGES_UBOOT_IMAGE
+# uboot trusted images are never used 
+# UBOOT_POST_INSTALL_IMAGES_HOOKS += UBOOT_INSTALL_IMAGES_TRUST_IMAGE
 
 endif # Rockchip modifications
