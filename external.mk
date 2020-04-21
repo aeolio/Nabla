fe1d85d42191f637c132037e0f14c1456663f8fb
iago <git@aeolio.de>
2020-04-21 16:58:08 +0200

    use absolute path on makefile include

--- external.mk
+++ external.mk
@@ -1,4 +1,1 @@
-# BR2_EXTERNAL_NABLA_PATH is enclosed in double quotes
-path = $(strip $(subst ",,$(BR2_EXTERNAL_NABLA_PATH)))
-#"))
-include $(sort $(wildcard $(path)/package/*/*.mk))
+include $(sort $(wildcard ~/br2-external/package/*/*.mk))
