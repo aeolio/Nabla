c21179af341831a650be141d00cd7b54770474d4
iago <git@aeolio.de>
2020-11-01 17:51:50 +0100

    update folve to latest version using fuse3

--- package/folve/folve.mk
+++ package/folve/folve.mk
@@ -4,19 +4,21 @@
 #
 ################################################################################
 
-FOLVE_VERSION = 84b92b49c8e64aaa6370753c3900f90eb6fef998
+FOLVE_VERSION = 874063e136a44d8f7a0066ced85c7793f44283d4
 FOLVE_SITE = https://github.com/hzeller/folve
 FOLVE_SITE_METHOD = git
 FOLVE_INSTALL_STAGING = YES
-FOLVE_DEPENDENCIES = flac libfuse libmicrohttpd libsndfile zita-convolver
+FOLVE_DEPENDENCIES = flac libfuse3 libmicrohttpd libsndfile zita-convolver
 FOLVE_LICENSE = GPL3
 FOLVE_LICENSE_FILES = COPYING
 
-#FOLVE_MAKE_ENV += " DESTDIR=$SYSROOT_PREFIX"
-#FOLVE_MAKE_ENV += " FFTW_PATH=$SYSROOT_PREFIX/usr"
+FOLVE_MAKE_ENV += " SNDFILE_INC= "
+FOLVE_MAKE_ENV += " SNDFILE_LIB=-lsndfile"
+FOLVE_MAKE_ENV += " FUSE_INC=-I$(STAGING_DIR)/usr/include/fuse3"
+FOLVE_MAKE_ENV += " FUSE_LIB=-lfuse3"
 
 define FOLVE_BUILD_CMDS
-	$(MAKE) CXX="$(TARGET_CXX)" LD="$(TARGET_LD)" -C $(@D)
+	$(MAKE) $(FOLVE_MAKE_ENV) CXX="$(TARGET_CXX)" LD="$(TARGET_LD)" -C $(@D)
 endef
 
 define FOLVE_INSTALL_TARGET_CMDS
