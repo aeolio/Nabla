1955609cede14422cb2d07d7b3b0db6ccdb6fc23
iago <git@aeolio.de>
2020-11-16 08:51:29 +0100

    correctj typos

--- package/syslinux/syslinux.mk
+++ package/syslinux/syslinux.mk
@@ -4,10 +4,8 @@
 #
 ################################################################################
 
-target = $(findstring rockpi4,$(BR2_DEFCONFIG))
-
-# syslinux.sys is needed to create a bootable disk image
-# copy the file syslinux.sys to image directory
+# ldlinux.sys is needed to create a bootable disk image
+# copy the file ldlinux.sys to the image directory
 ifeq ($(BR2_TARGET_SYSLINUX_LEGACY_BIOS),y)
 define SYSLINUX_INSTALL_LEGACY_BIOS_DRIVER
 	$(INSTALL) -D -m 0755 $(@D)/bios/core/ldlinux.sys \
