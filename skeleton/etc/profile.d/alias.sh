fd624c1162f3ed8c10d2666aaa0d2a987f56621f
iago <git@aeolio.de>
2020-11-16 08:08:10 +0100

    improve output of list-rt-priorities()

--- skeleton/etc/profile.d/alias.sh
+++ skeleton/etc/profile.d/alias.sh
@@ -1,7 +1,7 @@
 list_rt_priorities()
 {
 	ps | \
-	awk '/[[:digit:]+]/{ if ($5 != "ps") { cmd = "chrt -p " $1 " | sed \"s/.*: //g\" | tr \"\n\" \" \""; print $5; system(cmd); } }' | \
+	awk '/[[:digit:]+]/{ if ($5 != "ps") { cmd = "chrt -p " $1 " | sed \"s/.*: //g\" | tr \"\n\" \" \""; system(cmd); print "\t" $5; } }' | \
 	grep SCHED_FIFO
 }
 
