29abbe1d98c2696a8fa5e29b27d01106c96b21aa
iago <git@aeolio.de>
2021-01-17 13:49:19 +0100

    change version numbering scheme

--- package/folve/folve.mk
+++ package/folve/folve.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FOLVE_VERSION = 2019-11
+FOLVE_VERSION = 2019.11
 FOLVE_SOURCE = folve-$(FOLVE_VERSION).tar.gz
 FOLVE_SITE = $(call github,hzeller,folve,master)
 FOLVE_INSTALL_STAGING = YES
