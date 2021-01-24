034305644c456d5bbeacd11b380f4edfb40ccc1d
iago <git@aeolio.de>
2021-01-24 06:32:04 +0100

    fix error in install target hook

--- package/busybox/busybox.mk
+++ package/busybox/busybox.mk
@@ -6,6 +6,9 @@
 
 # modify configuration
 define BUSYBOX_MODIFY_INIT_CONFIG
-	patch $(TARGET_DIR)/etc/mdev.conf $(BR2_EXTERNAL)/package/busybox/0000-additional-mdev-configurations.patch
+	if [ -f "$(TARGET_DIR)/etc/mdev.conf" ]; then \
+		patch $(TARGET_DIR)/etc/mdev.conf \
+			$(BR2_EXTERNAL)/package/busybox/0000-additional-mdev-configurations.patch; \
+	fi
 endef
 BUSYBOX_POST_INSTALL_TARGET_HOOKS += BUSYBOX_MODIFY_INIT_CONFIG
