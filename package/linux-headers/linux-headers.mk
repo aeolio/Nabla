5252138639d75b71802d1d4c694521c803dc61e5
iago <git@aeolio.de>
2021-10-30 07:54:29 +0200

    package/linux-headers: simplify logic

--- package/linux-headers/linux-headers.mk
+++ package/linux-headers/linux-headers.mk
@@ -4,15 +4,11 @@
 #
 ################################################################################
 
-# RT patch for newer Linux versions contains also changes in sample code
+# RT patch for Linux versions from 4.19 contains also changes in sample code
 LINUX_HEADERS_EXCLUDES = configs *.bmp
-ifeq ($(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_4_4),y)
-LINUX_HEADERS_EXCLUDES += samples
-endif
 ifeq ($(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_3_18),y)
 LINUX_HEADERS_EXCLUDES += samples
 endif
-# RT patch for 4.19 kernel has patches in arm/configs
-ifeq ($(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_4_19),y)
-LINUX_HEADERS_EXCLUDES = *.bmp
+ifeq ($(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_4_4),y)
+LINUX_HEADERS_EXCLUDES += samples
 endif
