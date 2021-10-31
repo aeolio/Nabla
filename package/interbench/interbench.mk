6fb4363afa2f268f8081386e093a7dce5860c680
iago <git@aeolio.de>
2021-10-31 16:57:01 +0100

    package/interbench: move to private repository

--- package/interbench/interbench.mk
+++ package/interbench/interbench.mk
@@ -4,16 +4,10 @@
 #
 ################################################################################
 
-INTERBENCH_VERSION = 0.31
-INTERBENCH_SITE = $(call github,ckolivas,interbench,master)
-INTERBENCH_LICENSE = GPLv2
+INTERBENCH_VERSION = 0.36
+INTERBENCH_SITE = $(call github,aeolio,interbench,$(INTERBENCH_VERSION))
+INTERBENCH_LICENSE = GPLv3
 INTERBENCH_LICENSE_FILES = COPYING
-
-# interbench comes with precompiled binaries
-define INTERBENCH_REMOVE_OBJECTS
-	$(RM) -fr $(@D)/*.o interbench
-endef
-INTERBENCH_POST_EXTRACT_HOOKS += INTERBENCH_REMOVE_OBJECTS
 
 define INTERBENCH_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) \
