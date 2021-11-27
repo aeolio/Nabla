b10d1203f81d007864b71e35c30df06c3b50a228
iago <git@aeolio.de>
2021-11-27 08:17:05 +0100

    rename config to defaults

--- package/brutefir/brutefir.mk
+++ package/brutefir/brutefir.mk
@@ -13,6 +13,8 @@ BRUTEFIR_DEPENDENCIES = fftw-single fftw-double
 BRUTEFIR_MAKE_ENV += " DESTDIR=$STAGING_DIR"
 BRUTEFIR_MAKE_ENV += " FFTW_PATH=$STAGING_DIR/usr"
 
+BRUTEFIR_DEFAULTS_DIR += "$(BR2_EXTERNAL_NABLA_PATH)/package/brutefir/defaults"
+
 define BRUTEFIR_BUILD_CMDS
 	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" $(BRUTEFIR_MAKE_ENV) -C $(@D) all
 endef
@@ -22,8 +24,8 @@ define BRUTEFIR_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/brutefir $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/*.bfio $(TARGET_DIR)/usr/lib/brutefir
 	$(INSTALL) -m 0755 $(@D)/*.bflogic $(TARGET_DIR)/usr/lib/brutefir
-	$(INSTALL) -m 0755 $(BR2_EXTERNAL_NABLA_PATH)/package/brutefir/config/S95brutefir $(TARGET_DIR)/etc/init.d
-	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_NABLA_PATH)/package/brutefir/config/asound.conf $(TARGET_DIR)/etc/asound.conf
+	$(INSTALL) -m 0755 $(BRUTEFIR_DEFAULTS_DIR)/S95brutefir $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 0644 -D $(BRUTEFIR_DEFAULTS_DIR)/asound.conf $(TARGET_DIR)/etc/asound.conf
 endef
 
 $(eval $(generic-package))
