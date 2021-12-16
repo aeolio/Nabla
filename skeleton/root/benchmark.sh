71ffdc954d674dae695836f39683273e3a13c841
aeolio <git@aeolio.de>
2021-12-16 10:29:40 +0100

    skeleton: add option to benchmark script

--- skeleton/root/benchmark.sh
+++ skeleton/root/benchmark.sh
@@ -5,13 +5,16 @@ echo -n "uname = " && uname -r
 echo -n "tasks = " && ps | wc -l
 echo -n "free = " && free | awk '/Mem:/ { print $3 " /" ($2 - $7) }'
 case "$1" in
-  -i)
+  -c)
     # start interbench without console interaction
     interbench -r -u -W Audio < /dev/zero > /dev/null 2>&1 &
+    ;;
+  -w)
+    # start interbench without write benchmarks
+    interbench -r -u -w None -w Video -w X -w Burn -w Read -w Ring -W Audio
     ;;
   *)
     # start interbench normally
     interbench -r -u -W Audio
     ;;
 esac
-
