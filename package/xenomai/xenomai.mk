2f60c71d2a1f9aa2c59cf4f1490e0a899b03e875
aeolio <git@aeolio.de>
2021-12-23 12:19:47 +0100

    packages: add xenomai adaptions to enable building with version 3.1.y

---
+++ package/xenomai/xenomai.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# Fragment file for xenomai
+#
+################################################################################
+
+# these resolve warnings treated as errors when compiling with a glibc toolchain
+XENOMAI_CFLAGS += -Wno-stringop-overread
+XENOMAI_CFLAGS += -Wno-incompatible-pointer-types
+XENOMAI_CFLAGS += -Wno-deprecated-declarations
+XENOMAI_CFLAGS += -DHAVE_PTHREAD_MUTEXATTR_SETROBUST_NP
+
+XENOMAI_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) $(XENOMAI_CFLAGS)"
+
+# disable conflicting options in Linux kernel
+ifeq ($(BR2_PACKAGE_XENOMAI),y)
+define XENOMAI_LINUX_CONFIG_FIXUPS
+	$(call KCONFIG_DISABLE_OPT,CONFIG_PREEMPT_RT)
+endef
+endif
