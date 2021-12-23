0d78be38f1087e0febcc50b4368f2f08f5668b5f
aeolio <git@aeolio.de>
2021-12-23 08:45:17 +0100

    make kernel NFS configuration dependent on buildroot filesystem config

--- package/nfs-utils/nfs-utils.mk
+++ package/nfs-utils/nfs-utils.mk
@@ -4,6 +4,14 @@
 #
 ################################################################################
 
+# linux kernel needs NFS and NFSv3 if NFS daemon is selected
+ifeq ($(BR2_PACKAGE_NFS_UTILS_RPC_NFSD),y)
+define NFS_UTILS_LINUX_CONFIG_FIXUPS
+	$(call KCONFIG_ENABLE_OPT,CONFIG_NFSD)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_NFSD_V3)
+endef
+endif
+
 NFS_UTILS_LIBDIR = var/lib/nfs
 
 ifeq ($(BR2_PACKAGE_NFS_UTILS_RPC_NFSD),y)
