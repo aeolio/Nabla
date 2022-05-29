0b9674b4c4bf375b9b508a2df1a1a14578f5caba
aeolio <git@aeolio.de>
2022-05-29 07:49:50 +0200

    package/wpa_supplicant: remove unnecessary startup scripts

--- package/wpa_supplicant/wpa_supplicant.mk
+++ package/wpa_supplicant/wpa_supplicant.mk
@@ -9,3 +9,11 @@ define WPA_SUPPLICANT_REMOVE_CONFIG
 	rm -f $(TARGET_DIR)/etc/wpa_supplicant.conf
 endef
 WPA_SUPPLICANT_POST_INSTALL_TARGET_HOOKS += WPA_SUPPLICANT_REMOVE_CONFIG
+
+# remove ifupdown scripts that interfere with 
+# wpa_supplicant started from init script
+define WPA_SUPPLICANT_REMOVE_IFUPDOWN_SCRIPTS
+	rm -f $(TARGET_DIR)/etc/network/if-down.d/wpasupplicant
+	rm -f $(TARGET_DIR)/etc/network/if-up.d/wpasupplicant
+endef
+WPA_SUPPLICANT_POST_INSTALL_TARGET_HOOKS += WPA_SUPPLICANT_REMOVE_IFUPDOWN_SCRIPTS
