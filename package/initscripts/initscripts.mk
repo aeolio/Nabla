38c1c48c3b7e29204c52f88df0c97fd73c584861
aeolio <git@aeolio.de>
2022-07-10 07:07:54 +0200

    package/initscripts: be more selective when deleting init scripts

--- package/initscripts/initscripts.mk
+++ package/initscripts/initscripts.mk
@@ -4,14 +4,22 @@
 #
 ################################################################################
 
-INITSCRIPTS_STARTUP_FILE = $(TARGET_DIR)/etc/init.d/rcS
+INITSCRIPTS_DIRECTORY = $(TARGET_DIR)/etc/init.d
+INITSCRIPTS_STARTUP_FILE = $(INITSCRIPTS_DIRECTORY)/rcS
+
+# initscripts should not install S11modules unless BR2_SYSTEM_CONNECT_WIFI is set
+ifneq ($(BR2_SYSTEM_CONNECT_WIFI),y)
+define INITSCRIPTS_MODULES
+	rm -f $(INITSCRIPTS_DIRECTORY)/S11modules
+endef
+INITSCRIPTS_POST_INSTALL_TARGET_HOOKS += INITSCRIPTS_MODULES
+endif # BR2_SYSTEM_CONNECT_WIFI
 
 # modify central startup script
-define INISCRIPTS_BOOTLOG
-	echo $(INITSCRIPTS_STARTUP_FILE)
+define INITSCRIPTS_BOOTLOG
 	if [ -f "$(INITSCRIPTS_STARTUP_FILE)" ]; then \
 		patch $(INITSCRIPTS_STARTUP_FILE) \
 			$(BR2_EXTERNAL)/package/initscripts/0000-redirect-output-to-boot-log.patch; \
 	fi
 endef
-INITSCRIPTS_POST_INSTALL_TARGET_HOOKS += INISCRIPTS_BOOTLOG
+INITSCRIPTS_POST_INSTALL_TARGET_HOOKS += INITSCRIPTS_BOOTLOG
