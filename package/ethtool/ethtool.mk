a8fb520a6d31a07be17ee164ae2f6180d55fbdf8
aeolio <git@aeolio.de>
2023-01-06 16:19:09 +0100

    package/ethtool: fix missing _GNU_SOURCE

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -10,3 +10,8 @@ define ETHTOOL_LINUX_CONFIG_FIXUPS
 	$(call KCONFIG_ENABLE_OPT,CONFIG_ETHTOOL_NETLINK)
 endef
 endif
+
+# _GNU_SOURCE is needed for musl sys/types.h to define u_int32_t
+ifeq ($(BR2_TOOLCHAIN_USES_MUSL),y)
+ETHTOOL_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE"
+endif
