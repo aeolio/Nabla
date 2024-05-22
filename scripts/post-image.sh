cf4d293a2dc4bacca77b844d409968881c99e6cc
aeolio <git@aeolio.de>
2024-05-22 12:56:49 +0200

    scripts: use multiple generations for image backup

--- scripts/post-image.sh
+++ scripts/post-image.sh
@@ -11,11 +11,20 @@ _path=$BR2_EXTERNAL_NABLA_PATH/scripts
 ### backup project image files
 ###
 
-PROJECT_NAME=$(get_project_name)
 BACKUP_DIR=$BR2_EXTERNAL_NABLA_PATH/_backup/images
+PROJECT_NAME=$(get_project_name)
+GENERATIONS=7
 _SOURCE_DIR=$BINARIES_DIR
-_TARGET_DIR=$BACKUP_DIR/$PROJECT_NAME
+_TARGET_DIR=$BACKUP_DIR/$PROJECT_NAME/$(date +%Y%m%d%H%M%S)
 _EXCLUDE_PATTERN="--exclude=*.vfat --exclude=*.cpio --exclude=*.img"
 
 mkdir -p $_TARGET_DIR
 rsync -qrlt $_EXCLUDE_PATTERN --delete $_SOURCE_DIR/ $_TARGET_DIR/
+
+n=0
+for d in $(ls -d --time=birth $BACKUP_DIR/$PROJECT_NAME/[0-9]*); do
+	n=$((n+1))
+	if [ $n -gt $GENERATIONS ]; then
+		rm -fr $d
+	fi
+done
