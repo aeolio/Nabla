c4a5d6ba7893cb103724369d180cf878a0d267c7
aeolio <git@aeolio.de>
2024-07-12 10:50:20 +0200

    package/openssh: base sshd init.d script on buildroot template

---
+++ package/openssh/openssh.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# additional openssh logic
+#
+################################################################################
+
+OPENSSH_INIT_SCRIPT = $(TARGET_DIR)/etc/init.d/S50sshd
+
+# modify mdev configuration
+define OPENSSH_MODIFY_INIT_SCRIPT
+	if [ -f "$(OPENSSH_INIT_SCRIPT)" ]; then \
+		patch $(OPENSSH_INIT_SCRIPT) \
+			$(BR2_EXTERNAL)/package/openssh/0000-modify-startup-script.patch; \
+	fi
+endef
+OPENSSH_POST_INSTALL_TARGET_HOOKS += OPENSSH_MODIFY_INIT_SCRIPT
