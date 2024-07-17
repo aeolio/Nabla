a2e6237915bef51316bc9227a9b0e1ea7c122659
aeolio <git@aeolio.de>
2024-07-17 07:05:17 +0200

    package/mpd: base init.d script on buildroot template

--- package/mpd/mpd.mk
+++ package/mpd/mpd.mk
@@ -15,6 +15,17 @@ endef
 # this is necessary because fragment files do not get processed by inner-generic-package
 PACKAGES_USERS += $(MPD_USERS)$(sep)
 
+MPD_INIT_SCRIPT = $(TARGET_DIR)/etc/init.d/S95mpd
+
+# modify init.d configuration
+define MPD_MODIFY_INIT_SCRIPT
+	if [ -f "$(MPD_INIT_SCRIPT)" ]; then \
+		patch $(MPD_INIT_SCRIPT) \
+			$(BR2_EXTERNAL)/package/mpd/0000-modify-startup-script.patch; \
+	fi
+endef
+MPD_POST_INSTALL_TARGET_HOOKS += MPD_MODIFY_INIT_SCRIPT
+
 # if a base version patch directory exists, a symbolic link 
 # for the current version should also be present
 define MPD_PATCH_ASSURANCE
