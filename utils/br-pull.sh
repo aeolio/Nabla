8415c7cc189f241600068d4657360903f14e9ee3
aeolio <git@aeolio.de>
2025-10-31 05:15:44 +0100

    utils: collect all scripts in one place

---
+++ utils/br-pull.sh
@@ -0,0 +1,56 @@
+#/bin/sh
+
+instances="test/buildroot staging/buildroot buildroot"
+merge_instance="buildroot"
+merge_branch="nabla"
+
+BACKUP_CREATED=0
+
+backup_wip() {
+	if ! git diff-index --quiet HEAD -- ; then
+		git stash || exit 10
+		export BACKUP_CREATED=1
+	fi
+}
+
+restore_wip() {
+	if [ "$BACKUP_CREATED" = 1 ]; then
+		git stash apply || exit 11
+		unset BACKUP_CREATED
+	fi
+}
+
+pull_instance() {
+	printf "pull %s\n" $1
+	cd ~/$1
+	current_branch=$(git branch --show)
+	if [ $current_branch != 'master' ]; then
+		backup_wip
+		git checkout master || exit 1
+		git pull || exit 2
+		git checkout nabla || exit 3
+		restore_wip
+	else
+		git pull || exit 2
+	fi
+}
+
+merge_instance() {
+	cd ~/$1
+	current_branch=$(git branch --show)
+	if [ $current_branch == $merge_branch ]; then
+		printf "merge %s\n" $1
+		log_text="auto merge $(date --iso-8601='seconds')"
+		git merge -m "$log_text" origin/master
+	fi
+}
+
+for d in $instances
+do
+	current_dir=$(pwd)
+	pull_instance $d
+	if [ $d == $merge_instance ]; then
+		merge_instance $d
+	fi
+	cd $current_dir
+done
---
+++ utils/mkall.sh
@@ -0,0 +1,187 @@
+#!/bin/sh
+
+highlight="\33[38;5;0;48;5;214m"
+blackonwhite="\33[30;47m"
+reset="\33[m"
+
+_do_clean=0
+_do_deploy=0
+_do_make=0
+_do_purge=0
+_do_saveconfig=0
+
+linux_config=~/br2-external/Config.in.linux
+
+# remove duplicate packages from build directory
+# precondition: current directory == project directory
+purge_build_directory() {
+	for d in build/*; do
+		if [ -d $d ]; then
+			# strip package name
+			package_name=$(basename $d)
+			package_name=${package_name%-[0-9]*}
+			builds=$(echo build/${package_name}-[0-9]*)
+			if [ "$builds" != "build/${package_name}-[0-9]*" ]; then
+				current_version="19700101000000"
+				i=0
+				# identify most recent version
+				for b in $builds; do
+					# may have been removed already
+					if [ ! -d $b ]; then continue; fi
+					ts=$(ls -ld --time-style="+%Y%m%d%H%M%S" $b | awk '{print $6}')
+					if [ $ts -gt $current_version ]; then
+						current_version=$ts
+					fi
+					i=$(expr $i + 1)
+				done
+				if [ $i -gt 1 ]; then
+					for b in $builds; do
+						if [ ! -d $b ]; then continue; fi
+						ts=$(ls -ld --time-style="+%Y%m%d%H%M%S" $b | awk '{print $6}')
+						if [ $ts -ne $current_version ]; then
+							rm -fr $b && echo "$b removed"
+						fi
+					done
+				fi
+			fi
+		fi
+	done
+}
+
+# copy image file(s) to target system
+# precondition: current directory == project directory
+deploy_image_files() {
+	local rsync_options="--checksum --stats --times --verbose"
+	local transferred_files="Number of regular files transferred"
+	local mrw="mount -o remount,rw /media/boot"
+	local mro="mount -o remount,ro /media/boot"
+	local cfgfile=system.conf
+	if [ -e $cfgfile ]; then
+		. ./$cfgfile
+		local logfile=/tmp/$HOSTNAME.log
+		if ssh -o ConnectTimeout=2 -q root@$HOSTNAME true; then
+			printf ">>>   Image deployment to %s\n" $HOSTNAME
+			ssh root@$HOSTNAME $mrw
+			rsync $rsync_options $IMAGEFILES root@$HOSTNAME:$TARGETDIR 2>&1 > $logfile || exit 1
+			changes=$(awk 'BEGIN{FS=":"} /'"$transferred_files"'/ {gsub(/ /, "", $2); print $2}' $logfile)
+			if [ $changes -gt 0 ]; then
+				awk '{if ($0=="") exit; print $0}' $logfile
+				ssh root@$HOSTNAME "sync && reboot"
+			else
+				ssh root@$HOSTNAME $mro
+			fi
+			rm -f $logfile
+		fi
+	fi
+	}
+
+print_usage() {
+	echo "  use: $0 [option] { --exec=<command> | --exclude=<project> | --include=<project> }"
+	echo "  option ::= { --[build|clean|deploy|purge|saveconfig] }"
+}
+
+if [ -z "$1" ]; then
+	print_usage && exit 1;
+fi
+
+# TODO: projets with their own buildroot instance will update .config every time
+# use buildroot revision to determine if config file needs update
+revision=$(git -C ~/buildroot describe)
+# return to the current directory afterwards
+current_dir=$(pwd)
+# excluded projects
+exclusion_list=""
+# included projects
+targets=""
+
+for arg in "$@"
+do
+	case $arg in
+
+		--clean)
+		_do_clean=1
+		;;
+
+		--exec=*)
+		command=${arg#*=}
+		;;
+
+		--build)
+		_do_make=1
+		;;
+
+		--purge)
+		_do_purge=1
+		;;
+
+		--deploy)
+		_do_deploy=1
+		;;
+
+		--save*)
+		_do_saveconfig=1
+		;;
+
+		--include=*)
+		targets="$targets $(ls ~/${arg#*=}/.config)"
+		exclusion_list=
+		;;
+
+		--exclude=*)
+		exclusion_list="$exclusion_list ${arg#*=}"
+		;;
+
+		*)
+		print_usage
+		exit 1;
+		;;
+
+	esac
+done
+
+[ -n "$targets" ] || targets=$(ls ~/*/.config)
+for t in $targets; do
+	p=$(dirname $t)	# project directory
+	pn=${p#$HOME\/}	# project name
+	# ignore clean build directories
+	if ! [ -d $p/build ]; then continue; fi
+	# ignore specified projects
+	if (echo ${exclusion_list} | grep -q "$pn"); then continue; fi
+	echo "\t${highlight}###" $pn "###${reset}"
+	cd $p
+	# exclusively execute make target if specified
+	if [ -n "$command" ]; then
+		make $command
+		continue;
+	fi
+	# update .config if revision changed or modification /status change is newer
+	if	[ $(grep -c $revision .config) -ne 1 ] ||
+		[ $(stat -c%Y .config) -lt $(stat -c%Y $linux_config) ] ||
+		[ $(stat -c%Z .config) -lt $(stat -c%Z $linux_config) ]; then
+		make olddefconfig || exit $?
+	fi
+	# update buildroot and kernel configurations
+	if [ ${_do_saveconfig} -eq 1 ]; then
+		make savedefconfig
+		# make linux-update-defconfig
+		continue;
+	fi
+	# clear out target build folder
+	if [ ${_do_clean} -eq 1 ]; then
+		make clean
+	fi
+	# rebuild target
+	if [ ${_do_make} -eq 1 ]; then
+		make || exit $?
+		# rm +([0-9]) ## works with /bin/bash only
+	fi
+	# purge outdated package versions from build directory
+	if [ ${_do_purge} -eq 1 ]; then
+		purge_build_directory || exit $?
+	fi
+	# deploy image files
+	if [ ${_do_deploy} -eq 1 ]; then
+		deploy_image_files || exit $?
+	fi
+done
+cd ${current_dir}
