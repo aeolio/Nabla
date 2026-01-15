29547fd54b7d9ac413325c415b8aca22a11a4aff
aeolio <git@aeolio.de>
2026-01-15 07:15:02 +0100

    package/linux-headers: fix error deleting stale linux versions

--- package/linux-headers/linux-headers.mk
+++ package/linux-headers/linux-headers.mk
@@ -12,3 +12,17 @@ endif
 ifeq ($(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_4_4),y)
 LINUX_HEADERS_EXCLUDES += samples
 endif
+
+# remove stale build versions of linux and linux-headers
+define LINUX_HEADERS_REMOVE_STALE
+	find $(BUILD_DIR) -type d \
+		-regex .*/linux-[aedhrs-]*[0-9]*\.[0-9]*\.[0-9]* \
+		-prune \
+		-exec sh -c \
+			'f=$$(basename $$0); \
+			if [ "$${f##linux*-}" != "$(LINUX_VERSION)" ]; then \
+				rm -fr "$$0"; \
+			fi' \
+			"{}" \;
+endef
+LINUX_HEADERS_PRE_DOWNLOAD_HOOKS += LINUX_HEADERS_REMOVE_STALE
