2ab8a6a8a7a57a5ab2df9130d6ffebd39b9cb8d8
aeolio <15275159+aeolio@users.noreply.github.com>
2026-02-28 10:33:35 +0100

    scripts: post-image: introduce LIFO environment variable to control deletion of oldest /newest backup first

--- scripts/post-image.sh
+++ scripts/post-image.sh
@@ -19,13 +19,31 @@ _SOURCE_DIR=$BINARIES_DIR
 _TARGET_DIR=$BACKUP_DIR/$PROJECT_NAME/$(date +%Y%m%d%H%M%S)
 _EXCLUDE_PATTERN="--exclude=*.vfat --exclude=*.cpio --exclude=*.img"
 
+project_backup_list() {
+	find "$BACKUP_DIR/$PROJECT_NAME" -type d -regex ".*/[0-9]+" | sort "$1"
+}
+
+delete_backup_directories() {
+	n="$1"
+	for d in $(project_backup_list "$2"); do
+		n=$((n+1))
+		if [ "$n" -gt "$GENERATIONS" ]; then
+			rm -fr "$d"
+		fi
+	done
+}
+
+# LIFO environment variable controls if the oldest or newest entry
+# is deleted first if the number of generations is exceeded. Default
+# is to delete the oldest entry.
+
+if [ -n "$LIFO" ] && [ "$LIFO" -eq 1 ]; then
+	delete_backup_directories 1 -g
+fi
+
 mkdir -p "$_TARGET_DIR"
 rsync -qrlt "$_EXCLUDE_PATTERN" --delete "$_SOURCE_DIR/" "$_TARGET_DIR/"
 
-n=0
-for d in $(find "$BACKUP_DIR/$PROJECT_NAME" -type d -regex ".*/[0-9]+" | sort -r); do
-	n=$((n+1))
-	if [ "$n" -gt "$GENERATIONS" ]; then
-		rm -fr "$d"
-	fi
-done
+if [ -z "$LIFO" ] || [ "$LIFO" -eq 0 ]; then
+	delete_backup_directories 0 -r
+fi
