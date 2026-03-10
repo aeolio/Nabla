a96143b29a2ee33c4630f7192ce6139338866e30
aeolio <15275159+aeolio@users.noreply.github.com>
2026-03-10 08:43:27 +0100

    utils: backup.sh: put archives in correct location

--- utils/backup.sh
+++ utils/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-archive="br2-external_config_$(date +%Y%m%d-%H%M).tar.bz2"
+archive="_backup/$(date +%Y)/br2-external_config_$(date +%Y%m%d-%H%M).tar.bz2"
+
 source=""
 source="$source board/"
 source="$source configs/"
@@ -20,3 +21,4 @@ source="$source README"
 source="$source _backup/s87localopt/"
 
 tar cjf $archive $source
+echo "Created $(basename $archive)"
