afb48c5d429d86fed50e84b46a2339a508e964fa
aeolio <15275159+aeolio@users.noreply.github.com>
2026-06-02 13:06:02 +0200

    utils: backup: use multi-line string to define sources

--- utils/backup.sh
+++ utils/backup.sh
@@ -3,23 +3,24 @@
 archive_dir="_backup/$(date +%Y)"
 archive_file="$archive_dir/br2-external_config_$(date +%Y%m%d-%H%M).tar.bz2"
 
-source=""
-source="$source board/"
-source="$source configs/"
-source="$source package/"
-source="$source patches/"
-source="$source scripts/"
-source="$source skeleton/"
-source="$source utils/"
-
-source="$source Config.in"
-source="$source Config.in.linux"
-source="$source external.*"
-source="$source LICENSE"
-source="$source local.mk"
-source="$source README"
-
-source="$source _backup/s87localopt/"
+source=" \
+	board/ \
+	configs/ \
+	package/ \
+	patches/ \
+	scripts/ \
+	skeleton/ \
+	utils/ \
+\
+	Config.in \
+	Config.in.linux \
+	external.* \
+	LICENSE \
+	local.mk \
+	README \
+\
+	_backup/s87localopt/ \
+"
 
 mkdir -p "$archive_dir"
 # shellcheck disable=SC2086 # need separate parameters for tar
