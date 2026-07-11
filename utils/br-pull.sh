54f1a7c15cca8928432e49c88e13a0d94260090d
aeolio <15275159+aeolio@users.noreply.github.com>
2026-07-11 11:45:04 +0200

    utils: br-pull.sh: do not pull commits into a master branch with local changes

--- utils/br-pull.sh
+++ utils/br-pull.sh
@@ -1,14 +1,12 @@
-#/bin/sh
+#!/bin/sh
 
 instances="test/buildroot staging/buildroot buildroot"
-instance_to_merge="buildroot"
-branch_to_merge="nabla"
-
-BACKUP_CREATED=0
 
 FG_RED="\33[31m"
 FG_GREEN="\33[32m"
 RESET="\33[0m"
+
+git_status="Your branch is ahead of 'origin/master' by .* commits."
 
 backup_wip() {
 	if ! git diff-index --quiet HEAD -- ; then
@@ -26,35 +24,41 @@ restore_wip() {
 
 pull_instance() {
 	current_branch=$(git branch --show)
-	if [ $current_branch != 'master' ]; then
+	if [ "$current_branch" != 'master' ]; then
 		git checkout master || exit 1
 		git pull || exit 2
-		git checkout $current_branch || exit 3
-	else
+		git checkout "$current_branch" || exit 3
+		export NEEDS_MERGING=1
+	elif ! git status | grep -q "$git_status"; then
 		git pull || exit 2
+	else
+		printf "${FG_RED}  skipped due to open change requests${RESET}\n"
 	fi
 }
 
 merge_instance() {
 	current_branch=$(git branch --show)
-	if [ $current_branch != 'master' ]; then
-		printf "${FG_GREEN}Merge %s${RESET}\n" $1
+	if [ "$current_branch" != 'master' ]; then
+		printf "${FG_GREEN}Merge %s${RESET}\n" "$1"
 		log_text="auto merge $(date --iso-8601='seconds')"
 		git merge -m "$log_text" origin/master || exit 4
+		unset NEEDS_MERGING
 	fi
 }
 
+BACKUP_CREATED=0
+NEEDS_MERGING=0
 current_dir=$(pwd)
 
 for d in $instances
 do
-	cd ~/$d
-	printf "${FG_RED}Pull %s${RESET}\n" $d
+	cd "$HOME/$d" || exit 5
+	printf "${FG_RED}Pull %s${RESET}\n" "$d"
 	backup_wip
-	pull_instance $d
-	if [ $d == $instance_to_merge ]; then
-		merge_instance $d
+	pull_instance "$d"
+	if [ "$NEEDS_MERGING" = 1 ]; then
+		merge_instance "$d"
 	fi
 	restore_wip
-	cd $current_dir
+	cd "$current_dir" || exit 6
 done
