a607899c3e55c861811fa55529e760320545269b
aeolio <15275159+aeolio@users.noreply.github.com>
2026-08-22 07:59:32 +0200

    package/pihole-pi-hole: fix missing sql command replacement

--- package/pihole-pi-hole/pihole-pi-hole.mk
+++ package/pihole-pi-hole/pihole-pi-hole.mk
@@ -17,6 +17,14 @@ PIHOLE_PI_HOLE_TEMPLATE_DIR = advanced/Templates
 PIHOLE_PI_HOLE_DB_INIT_SQL = $(PIHOLE_PI_HOLE_TEMPLATE_DIR)/gravity.db.sql
 PIHOLE_PI_HOLE_DB_COPY_SQL = $(PIHOLE_PI_HOLE_TEMPLATE_DIR)/gravity_copy.sql
 
+# replace sqlite dot-command with pragma
+define PIHOLE_PI_HOLE_FIX_SQL
+	$(SED) \
+		'/.timeout/ s/.timeout \([0-9]*\)/PRAGMA busy_timeout =\1;/' \
+		$(@D)/$(PIHOLE_PI_HOLE_DB_COPY_SQL)
+endef
+PIHOLE_PI_HOLE_POST_BUILD_HOOKS += PIHOLE_PI_HOLE_FIX_SQL
+
 define PIHOLE_PI_HOLE_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(PIHOLE_PI_HOLE_TARGET_DIR)/$(PIHOLE_PI_HOLE_TEMPLATE_DIR)
 	$(INSTALL) -m 0644 -D $(@D)/$(PIHOLE_PI_HOLE_DB_INIT_SQL) \
